cb716627f43b93532022cd9df9c4cfbc9527bc7c
Sergei <githubofpehu@mail.ru>
2021-05-26 16:35:48 +0500

    Приблизительная обработка комманды start, пример загрузки группы фото

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import config
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InputMediaPhoto, InputTextMessageContent
+from aiogram.utils.markdown import text
 
 #Задаем уровень логов
 logging.basicConfig(level=logging.INFO)
@@ -11,10 +13,23 @@ bot = Bot(token=config.API_TOKEN)
 dp = Dispatcher(bot)
 
 # Эхо метод
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    answer = text('Привет, я помогу найти тебе квартиру на основе введёной информации', sep='\n')
+    await message.answer(answer)
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+
+    answer = text('Привет, я помогу найти тебе квартиру на основе введёной информации', sep='\n')
+    photo = ["https://cdn-p.cian.site/images/40/455/601/1065540484-1.jpg",
+             "https://cdn-p.cian.site/images/55/670/501/solnechnyy-chelyabinsk-jk-floor-plan-1050765569-6.jpg",
+             "https://cdn-p.cian.site/images/40/455/601/1065540497-1.jpg"]
+    media = []
+    for item in photo:
+        media.append(InputMediaPhoto(item))
+    await bot.send_message(message.from_user.id, answer)
+    await bot.send_media_group(message.from_user.id, media)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
